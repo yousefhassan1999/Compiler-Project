cbe760566abb41fd5c7b19da799c38d816e00440
AhmedAbdallah142 <ahmed.abdalla_142@yahoo.com>
2022-11-18 22:16:46 +0200

    Create NFA destructor

--- Compiler/src/main.cpp
+++ Compiler/src/main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <fstream>
 #include "src/lexicalAnalysis/lexicalGenerator.h"
 #include "src/lexicalAnalysis/nfaGraphGenerator/NFAGenerator.h"
@@ -9,7 +8,6 @@ int main() {
     LexicalGenerator ob;
     ob.getLexicalRules()->readFileContent(".././lexical rules.txt");
     NFAGenerator nfaGenerator(ob.getLexicalRules());
-    nfaGenerator.generateNFA();
     nfaGenerator.getNFARoot();
     return 0;
 }
