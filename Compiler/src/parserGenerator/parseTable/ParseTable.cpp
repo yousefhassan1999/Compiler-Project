19af11b6c361f5ac9cb69ff6ecaba347966b9311
AhmedAbdallah142 <ahmed.abdalla_142@yahoo.com>
2022-12-18 23:04:40 +0200

    Create Follows

--- Compiler/src/parserGenerator/parseTable/ParseTable.cpp
+++ Compiler/src/parserGenerator/parseTable/ParseTable.cpp
@@ -2,4 +2,73 @@
 // Created by ahmed on 12/17/2022.
 //
 
+#include <sstream>
 #include "ParseTable.h"
+
+#define EPSILON "Epsilon"
+
+using namespace std;
+
+void ParseTable::createParseTable() {
+    createFirst();
+    createFollows();
+
+}
+
+string ParseTable::getRule(string nonTerminal, char terminal) {
+    return {};
+}
+
+
+void ParseTable::createFollows() {
+    follows[startSymbol].insert("'$'");
+    int attempts = 10;
+    while (attempts--) {
+        for (auto rule: parserRules) {
+            for (const auto &nextRule: *rule.GetRHS()) {
+                auto words = split(nextRule);
+                for (int i = 0; i < words.size(); i++) {
+                    if (words[i][0] != '\'') { // check non-terminal
+                        bool addFollow = true;
+                        int j = i+1;
+                        while (j != words.size() && addFollow) {
+                            addFollow = false;
+                            if (words[i + 1][0] == '\'') { // check terminal
+                                follows[words[i]].insert(words[i + 1]);
+                                continue;
+                            }
+                            //add first except epsilon
+                            for (const string &firstElement: first[words[i + 1]]) {
+                                if (firstElement == EPSILON) {
+                                    addFollow = true;
+                                    continue;
+                                }
+                                follows[words[i]].insert(firstElement);
+                            }
+                        }
+                        if (addFollow && words[i] != rule.GetNonTerminal()){
+                            // add follows
+                            for (const string& followElement: follows[rule.GetNonTerminal()]) {
+                                follows[words[i]].insert(followElement);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+void ParseTable::createFirst() {
+
+}
+
+vector<string> ParseTable::split(const string &str) {
+    vector<string> res;
+    istringstream stream(str);
+    string word;
+    while (stream >> word) {
+        res.push_back(word);
+    }
+    return res;
+}
--- Compiler/src/parserGenerator/parseTable/ParseTable.h
+++ Compiler/src/parserGenerator/parseTable/ParseTable.h
@@ -9,14 +9,43 @@
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
+#include <list>
+#include <vector>
+#include "src/parserGenerator/CFGRules/CFGContainer.h"
 
 class ParseTable {
 private:
     std::unordered_map<std::string, int> nonTerminalIndices;
-    std::unordered_set<char>* first = new std::unordered_set<char>[10];
-    std::unordered_set<char>* follows = new std::unordered_set<char>[10];
+    std::unordered_map<std::string, int> terminalIndices;
+    std::string startSymbol;
+    std::unordered_map<std::string, std::unordered_set<std::string>> first;
+    std::unordered_map<std::string, std::unordered_set<std::string>> follows;
+    std::list<CFGContainer> parserRules;
+
+    static std::vector<std::string> split(const std::string& str);
+
+    void createFirst();
+    void createFollows();
+    void createParseTable();
 
 public:
+    explicit ParseTable(const std::list<CFGContainer>& rules) {
+        startSymbol = rules.front().GetNonTerminal();
+        int i =0;
+        for (const auto& rule:rules) {
+            nonTerminalIndices.insert({rule.GetNonTerminal(),i});
+        }
+        parserRules = rules;
+        createParseTable();
+    }
+
+//    ~ParseTable(){
+//        delete first;
+//        delete follows;
+//    }
+
+    std::string getRule(std::string nonTerminal, char terminal)
+
 
 };
 
